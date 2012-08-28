148374776dfb28f23b699193ba5aa20f651efed1
cainmodyo <abel.carvajalmodyo.com>
2012-08-28 14:30:55 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CainComunity::Application.config.secret_token = '9c403fbcf30faf45316d30fc288dc065e246afa3bb806766f706d2f125714e66c2a652bda9b358c99563303505ce47005f50f719e1d2b0499e4b28cf337c592c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CainComunity::Application.config.session_store :cookie_store, key: '_cain-comunity_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CainComunity::Application.config.session_store :active_record_store
