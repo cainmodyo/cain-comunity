6fd672bb0b23e813d004a27f6258c1cee4b488a8
cainmodyo <abel.carvajalmodyo.com>
2012-08-28 14:31:34 -0400

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CainComunity::Application.config.secret_token = '9c403fbcf30faf45316d30fc288dc065e246afa3bb806766f706d2f125714e66c2a652bda9b358c99563303505ce47005f50f719e1d2b0499e4b28cf337c592c'
+CainComunity::Application.config.secret_token = '2051eb64cbc2217fc03246766c9b55026a5e4a4ad62009cbada26566825ea3e559d6105d8f5403707ec7752375911a7eb257634773923c55f96c8e776473a182'
