fb2caebf4d9671af5e4960521bdf5f85c763ad28
cainmodyo <abel.carvajalmodyo.com>
2012-08-29 19:24:23 -0400

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    authorize! :index, @user, :message => 'Not authorized as an administrator.'
+    authorize! :index, @user, :message => 'Not authorized as no administrator.'
     @users = User.all
   end
 
   def show
+    authorize! :index, @user, :message => 'Not authorized as no administrator.'
     @user = User.find(params[:id])
   end
 
